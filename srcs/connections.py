a2fde5d8638c3ecd501e62ae5e4d0f7fb1835d09
Duda0402 <eduseragioli@gmail.com>
2026-06-12 11:26:22 +0100

    the connection is done with the docstrings and type hints

--- srcs/connections.py
+++ srcs/connections.py
@@ -4,22 +4,26 @@ from base import Base
 
 
 class Connection(Base):
+    """Represents a directional link between two zones with capacity constraints."""
 
     def __init__(self, name: str, zone_a: Zone, zone_b: Zone, max_link_capacity: int = 1) -> None:
-        self.name = name
-        self.zone_a = zone_a
-        self.zone_b = zone_b
-        self.max_link_capacity = max_link_capacity
-        self.drone_transit: list = []
+        """Initialize a connection between two zones"""
+        self.name: str = name
+        self.zone_a: Zone = zone_a
+        self.zone_b: Zone = zone_b
+        self.max_link_capacity: int = max_link_capacity
+        self.drone_transit: list[Drone] = []
         self.temp_blocked: bool = False
 
     def has_capacity(self) -> bool:
+        """Check whether this connection can accept more drones in transit."""
         if len(self.drone_transit) >= self.max_link_capacity:
             return False
         else:
             return True
 
     def add_drone(self, drone_actual: Drone) -> bool:
+        """Add a drone to this connection's transit list if capacity allows."""
         if self.has_capacity() is False:
             return False
         else:
@@ -27,6 +31,7 @@ class Connection(Base):
             return True
 
     def remove_drone(self, drone_actual: Drone) -> bool:
+        """Remove a drone from this connection's transit list."""
         if drone_actual not in self.drone_transit:
             return False
         else:
